bd0621011788d1f76ff20799815f23a4ff9de0cc
ozturksahinyetisir <108059951+ozturksahinyetisir@users.noreply.github.com>
2024-11-28 22:38:15 +0300

    Skills page & main.js added
    main.js added and tried scroll listener first time.

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-import 'bootstrap';
+console.log('Custom JS Loaded');
+
+window.addEventListener("hashchange", () => {
+    const sectionIndicator = document.getElementById("section-indicator");
+    const currentHash = window.location.hash.replace("#", "") || "Home"; 
+    sectionIndicator.textContent = currentHash.charAt(0).toUpperCase() + currentHash.slice(1);
+    console.log('hashchange');
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sectionIndicator = document.getElementById("section-indicator");
+    const currentHash = window.location.hash.replace("#", "") || "Home"; 
+    sectionIndicator.textContent = currentHash.charAt(0).toUpperCase() + currentHash.slice(1);
+});
+
+const navLinks = document.querySelectorAll('.nav-link');
+const currentUrl = window.location.pathname;
+
+navLinks.forEach(link => {
+  if (link.href.includes(currentUrl)) {
+    link.classList.add('active');
+  } else {
+    link.classList.remove('active');
+  }
+});
+
+function updateSectionOnScroll() {
+    const sections = document.querySelectorAll("section");
+    let currentSection = "Home";
+
+    sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= 0 && rect.bottom >= 0) {
+            currentSection = section.id.charAt(0).toUpperCase() + section.id.slice(1);
+        }
+    });
+
+    const sectionIndicator = document.getElementById("section-indicator");
+    sectionIndicator.textContent = currentSection;
+}
+
+
+window.addEventListener("scroll", updateSectionOnScroll);
+
+
+updateSectionOnScroll();
